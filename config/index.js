862b0c718f995f19227794d14078f0d31b92064c
robynainsley21 <robynainsley2i@gmail.com>
2024-08-07 14:48:16 +0200

    DB Connection

---
+++ config/index.js
@@ -0,0 +1,19 @@
+import { createPool } from "mysql2";
+import "dotenv/config";
+
+let connection = createPool({
+  host: process.env.hostDb,
+  user: process.env.userDb,
+  password: process.env.pwdDb,
+  database: process.env.dbName,
+  multipleStatements: true,
+  connectionLimit: 30,
+});
+
+connection.on('connection', (pool) => {
+    if(!pool) throw new Error('Database connection failed');
+})
+
+export {
+    connection
+}
